21fdf97cb559588d539a7399c6d0ae2a663bac20
Tom Zhang <tianze.zhang@gmail.com>
2022-06-18 14:41:44 +0100

    commit2

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -9,11 +9,13 @@ import javax.sql.DataSource;
 @Configuration
 public class PersistenceConfiguration {
     @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/conference_app");
-        System.out.println("My custom datasource bean has been initialized and set");
-        return builder.build();
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
 }
